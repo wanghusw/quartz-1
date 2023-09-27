075f76f8e37b1c7f255456d215b58c01e11817d3
Hu Wang <wanghusw@gmail.com>
2023-09-27 17:31:39 +0800

    2

--- quartz.config.ts
+++ quartz.config.ts
@@ -9,7 +9,7 @@ const config: QuartzConfig = {
     analytics: {
       provider: "plausible",
     },
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "wanghusw.github.io/quartz-1",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
